516dd875135bcc06ca2cc9f2072fd1dbfbbe6e13
Nikita Gusev <nikita.gusev.1987@gmail.com>
2015-10-03 21:57:01 +0200

    Adjusted script

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# wix-template
-Simple template to automate MSI creation
+# Wix Template
+Simple template to automate MSI creation.
+
+# Example
+PS:> .\createInstaller.ps1 -DirectoryToCollect Data -ApplicationName "Test App" -InstallationFile "install.ps1"
+
+DirectoryToCollect[Mandatory]: path to folder to collect files
+ApplicationName[Mandatory]: installer and application name
+InstallationFile[Optional]: powershell file to be run after installation
--- src/createInstaller.ps1
+++ src/createInstaller.ps1
@@ -1,25 +1,25 @@
 PARAM 
 (
 	[Parameter(Mandatory=$true)]
-	[string] $DirectoryToCollect,	
+	[string] $DirectoryToCollect,
 	[Parameter(Mandatory=$true)]
-	[string] $InputWixFile,
-	[Parameter(Mandatory=$true)]
-	[string] $OuputName,
-	[Parameter(Mandatory=$true)]
-	[string] $ComponentGroup,
-	[Parameter(Mandatory=$true)]
-	[string] $DirectoryID
-	
+	[string] $ApplicationName,	
+	[Parameter(Mandatory=$false)]
+	[string] $InputWixFile = "application.wxs",	
+	[Parameter(Mandatory=$false)]
+	[string] $InstallationFile = "None"	
 )
 
+$env:ApplicationName = "$ApplicationName"
+$env:InstallScriptFile = "$InstallationFile"
+
 #$env:Path += ";C:\Program Files (x86)\WiX Toolset v3.9\bin"
-heat.exe dir "$DirectoryToCollect" -cg "$ComponentGroup" -gg -scom -sreg -sfrag -srd -dr "$DirectoryID" -out "tempHeat.wxs"
+heat.exe dir "$DirectoryToCollect" -cg "FilesToBeInstalled" -gg -scom -sreg -sfrag -srd -dr "INSTALLLOCATION" -out "tempHeat.wxs"
 candle.exe "$InputWixFile" -out "tempMain.wixobj"
 candle.exe "tempHeat.wxs" -out "tempHeat.wixobj"
-light.exe -b "$DirectoryToCollect" "tempMain.wixobj" "tempHeat.wixobj" -ext WixUtilExtension -out "$OuputName.msi"
+light.exe -b "$DirectoryToCollect" "tempMain.wixobj" "tempHeat.wixobj" -ext WixUtilExtension -out "$ApplicationName.msi"
 
 Remove-Item "tempMain.wixobj"
 Remove-Item "tempHeat.wixobj"
 Remove-Item "tempHeat.wxs"
-Remove-Item "$OuputName.wixpdb"
+Remove-Item "$ApplicationName.wixpdb"
